e7db5383bc45f0fdde96b70e2458edd1a149b285
vinckr <vincent@ory.sh>
2020-10-02 12:42:15 +0200

    feat: change action
    Trying to change the action, fiddling around with stuff

--- index.js
+++ index.js
@@ -1,16 +1,35 @@
 
 const core = require('@actions/core');
 const github = require('@actions/github');
+const { Octokit } = require("@octokit/rest");
+const { request } = require("@octokit/request");
+
 
 try {
-  // `who-to-greet` input defined in action metadata file
-  const nameToGreet = core.getInput('who-to-greet');
-  console.log(`Hello ${nameToGreet}!`);
-  const time = (new Date()).toTimeString();
-  core.setOutput("time", time);
+
+        //start
+        
+
+    async function getContribData() {
+        console.log('starting getting data');
+        const result = await request("GET /repos/:owner/:repo/contributors", {
+            headers: {
+              authorization: "97dcf1e91e7c97a1800c41f512e3ceddf128dead",
+            },
+            owner: "ory",
+            repo: "kratos",
+          });
+          console.log(`${result.data.length} repos found.`);
+          console.log(`result: ${result.data}`);
+          console.log('end getting data');
+    }
+        //end
+
+getContribData();
+
   // Get the JSON webhook payload for the event that triggered the workflow
   const payload = JSON.stringify(github.context.payload, undefined, 2)
   console.log(`The event payload: ${payload}`);
 } catch (error) {
   core.setFailed(error.message);
-}
+} 
