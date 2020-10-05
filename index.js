f3f30be5a169644b9942f395bccbc90d6055ef10
vinckr <vincent@ory.sh>
2020-10-05 11:48:55 +0200

    feat: javascript github action fixes
    Huge thanks to @zepatrik for your suggestions!
    I think this has brought me a good step further.
    Unfortunately now I get this error:
    `(node:10320) UnhandledPromiseRejectionWarning: HttpError: request to https://api.github.comory/?0=k&1=r&2=a&3=t&4=o&5=s failed, reason: getaddrinfo ENOTFOUND api.github.comory`

--- index.js
+++ index.js
@@ -5,11 +5,26 @@ const { Octokit } = require("@octokit/rest");
 const { request } = require("@octokit/request");
 
 
-try {
+const octokit = new Octokit({
+    auth: "c65e4e7471903d2fd5cf6e6d975711f8288f063b",
+    userAgent: 'actions test',
+    timeZone: 'Europe/Amsterdam',
+    baseUrl: 'https://api.github.com',
 
-        //start
-        
+    log: {
+        debug: () => {},
+        info: () => {},
+        warn: console.warn,
+        error: console.error
+      },
+      request: {
+        agent: undefined,
+        fetch: undefined,
+        timeout: 0
+      }
+    })
 
+<<<<<<< Updated upstream
     async function getContribData() {
         console.log('starting getting data');
         const result = await request("GET /repos/:owner/:repo/contributors", {
@@ -33,3 +48,15 @@ getContribData();
 } catch (error) {
   core.setFailed(error.message);
 } 
+=======
+async function TestRequest() {
+  const TestData = octokit.repos.listContributors({
+    owner: 'ory',
+    repo: 'kratos',
+  });
+  return {TestData}
+}
+console.log('starting getting data');
+console.log(TestRequest);
+console.log('end getting data');
+>>>>>>> Stashed changes
